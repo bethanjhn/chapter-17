7b9d7e2ffc081482ca95924ff4a4db7598b15488
bethanjhn <46195797+bethanjhn@users.noreply.github.com>
2019-07-30 12:41:05 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,22 +10,23 @@ import lxml.html
 
 # scrape_table function: gets passed an individual page to scrape
 def scrape_table(root):
-    rows = root.cssselect("table.wikitable tr")  # selects all <tr> blocks within <table class="data">
+    rows = root.cssselect("table.Trolley.table tr")  # selects all <tr> blocks within <table class="data">
     for row in rows:
         # Set up our data record - we'll need it later
         record = {}
         table_cells = row.cssselect("td")
         if table_cells: 
-#             record['Release date'] = table_cells[0].text
-            record['Artist'] = table_cells[1].text
-            record['Album'] = table_cells[2].text
-            record['Genre'] = table_cells[3].text
-            record['Label'] = table_cells[4].text
+            record['Date'] = table_cells[0].text
+            record['Hospital'] = table_cells[1].text
+            record['Region'] = table_cells[2].text
+            record['Trolley Total'] = table_cells[3].text
+            record['Ward Total'] = table_cells[4].text
+
 
             # Print out the data we've gathered
             print record, '------------'
             # Finally, save the record to the datastore - 'Artist' is our unique key
-            scraperwiki.sqlite.save(["Artist"], record)
+            scraperwiki.sqlite.save(["Hospital"], record)
         
 # # scrape_and_look_for_next_link function: calls the scrape_table
 # # function, then hunts for a 'next' link: if one is found, calls itself again
@@ -41,10 +42,7 @@ def scrape_and_look_for_next_link(url):
         print next_url
         scrape_and_look_for_next_link(next_url)
 
-# ---------------------------------------------------------------------------
-# START HERE: define your starting URL - then 
-# call a function to scrape the first page in the series.
-# ---------------------------------------------------------------------------
-base_url = 'https://paulbradshaw.github.io'
-starting_url = urlparse.urljoin(base_url, '/scraping-for-everyone/webpages/example_table_1.html')
+  # START HERE: define your starting URL - then call a function to scrape it
+starting_url='http://inmo.ie/6022'
 scrape_and_look_for_next_link(starting_url)
+
